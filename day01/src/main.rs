8f568612e416e3d5a405b67b34aacf0c79b41c9a
mirnaxvb <m.vanbalgooi@sociallbue.com>
2022-12-01 22:36:30 +0100

    day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let data = include_str!("../input.txt")
+        .lines();
+    let mut biggest = 0;
+    let mut t: i32 = 0;
+
+    for line in data {
+        if line.is_empty() {
+            if t >= biggest {
+                biggest = t
+            }
+            t = 0;
+            continue;
+        }
+
+       t += line.parse::<i32>().unwrap();
+       println!("Total: {}", t);  
+    }
+
+    println!("Biggest: {}", biggest); 
 }
